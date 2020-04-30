56c302f6a7effdf864b3dd5ee0ab46a3da323dbb
FloezeTv <mathematikschlumpf@gmail.com>
2020-04-30 19:15:37 +0200

    Initial commit

---
+++ Wolf und Schafe/src/tv/floeze/wolfundschafe/WolfUndSchafe.java
@@ -0,0 +1,14 @@
+package tv.floeze.wolfundschafe;
+
+public class WolfUndSchafe {
+
+	public WolfUndSchafe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
